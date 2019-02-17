0f86ebb57d1cc79e1b1e9c73260dfcb2da04f0b7
eliwe <eliwen@gmx.at>
2019-02-17 16:14:29 +0100

    dieStaerkeistgenauaufdemDurchscnitt() Test erfolgreich

--- src/main/java/at/spengergasse/MyTest.java
+++ src/main/java/at/spengergasse/MyTest.java
@@ -6,6 +6,19 @@ import org.junit.jupiter.api.Test;
 
 public class MyTest {
 
+    @Test
+    public void dieStaerkeistgenauaufdemDurchscnitt() {
+        Logik tester = new Logik();
 
+        assertEquals("Die Stärke Ihres Händedrucks entspricht genau dem Durchschnitt von Personen in Ihrem Alters und mit Ihrer Größe.", tester.SterberisikoMethode(150,27.8f,17));
+        assertEquals("Die Stärke Ihres Händedrucks entspricht genau dem Durchschnitt von Personen in Ihrem Alters und mit Ihrer Größe.", tester.SterberisikoMethode(180,33.8f,17));
+        assertEquals("Die Stärke Ihres Händedrucks entspricht genau dem Durchschnitt von Personen in Ihrem Alters und mit Ihrer Größe.", tester.SterberisikoMethode(150,29.1f,20));
+        assertEquals("Die Stärke Ihres Händedrucks entspricht genau dem Durchschnitt von Personen in Ihrem Alters und mit Ihrer Größe.", tester.SterberisikoMethode(180,35.0f,20));
+
+        assertEquals("Die Stärke Ihres Händedrucks entspricht genau dem Durchschnitt von Personen in Ihrem Alters und mit Ihrer Größe.", tester.SterberisikoMethode(150,27.8f,19));
+        assertEquals("Die Stärke Ihres Händedrucks entspricht genau dem Durchschnitt von Personen in Ihrem Alters und mit Ihrer Größe.", tester.SterberisikoMethode(180,33.8f,19));
+        assertEquals("Die Stärke Ihres Händedrucks entspricht genau dem Durchschnitt von Personen in Ihrem Alters und mit Ihrer Größe.", tester.SterberisikoMethode(150,29.1f,24));
+        assertEquals("Die Stärke Ihres Händedrucks entspricht genau dem Durchschnitt von Personen in Ihrem Alters und mit Ihrer Größe.", tester.SterberisikoMethode(180,35.0f,24));
+    }
 
 }
